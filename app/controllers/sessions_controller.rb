0596e92bc358fc061dc19cf400dbadf92fecf172
KelloggCoder <keval.sheth@kellogg.northwestern.edu>
2026-06-11 17:16:18 +0000

    Added routes for logon and logout. Hide passwords while in transit (typing / filtered in log)

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,18 @@ class SessionsController < ApplicationController
   end
 
   def create
+  @user = User.find_by({ "email" => params["email"] })
+    if @user
+    # check secret password
+    else
+    redirect_to "/sessions/new"
+  end
   end
 
   def destroy
+    flash["notice"] = "Goodbye."
+    #session["user_id"] = nil
+    redirect_to "/sessions/new"
   end
 end
   
