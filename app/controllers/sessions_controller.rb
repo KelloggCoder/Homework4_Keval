9b183e4c0013aa7e71cfec9dea531a2927b025f5
KelloggCoder <keval.sheth@kellogg.northwestern.edu>
2026-06-11 17:51:09 +0000

    Checking email and password to authenticate login. Display messages for better end user experience. Have not yet encrypted.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,12 +3,20 @@ class SessionsController < ApplicationController
   end
 
   def create
-  @user = User.find_by({ "email" => params["email"] })
-    if @user
-    # check secret password
-    else
-    redirect_to "/sessions/new"
-  end
+   @user = User.find_by({ "email" => params["email"] })
+      if @user
+        if @user["password"] == params["password"]
+          # login the user
+          flash["notice"] = "You've logged in."
+          redirect_to "/"
+        else
+          flash["notice"] = "Incorrect Password."
+          redirect_to "/sessions/new"
+        end
+      else
+      flash["notice"] = "Incorrect Email."
+      redirect_to "/sessions/new"
+      end
   end
 
   def destroy
